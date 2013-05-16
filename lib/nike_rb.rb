9208b07c2686c21ede4de83317f8bafb5c07ea14
Kevin Wanek <k@dmcy.us>
2013-05-16 15:40:06 -0600

    added more nike API endpoints to gem (w/ options)

--- lib/nike_rb.rb
+++ lib/nike_rb.rb
@@ -20,35 +20,49 @@ module NikeRb
 			@access_token = access_token
 		end
 
-		
+
 
 	end
 
 	class Api
-
+		
 		def initialize(access_token)
 			@access_token = access_token
 		end
 
-		def activities(options={})
-	  	params = "#{API_URI}/me/sport/activities?access_token=#{@access_token}"
-	  	if options[:count]
-	  		params << "&count=#{options[:count]}"
-	  	end
+		def sport_data
+			params = "#{API_URI}/me/sport?access_token=#{@access_token}"
 
 			response = HTTParty.get(params, headers: HEADERS)
-			body = JSON.parse response.body
+			JSON.parse response.body
+		end
 
-			return body
+		def activities(options={})
+			if options[:start_date]
+				raw_start_date = options[:start_date]
+				start_date = raw_start_date.to_date.strftime("%Y-%e-%m")
+			end
+
+			if options[:end_date]
+				raw_end_date = options[:end_date]
+				end_date = raw_end_date.to_date.strftime("%Y-%e-%m")
+			end
+
+	  	params = "#{API_URI}/me/sport/activities?access_token=#{@access_token}"
+	  	params << "&count=#{options[:count]}" if options[:count]
+	  	params << "&offset=#{options[:offset]}" if options[:offset]
+	  	params << "&startDate=#{start_date}" if start_date
+	  	params << "&endDate=#{end_date}" if end_date
+
+			response = HTTParty.get(params, headers: HEADERS)
+			JSON.parse response.body
 		end
 
 		def activity_detail(id)
 			params = "#{API_URI}/me/sport/activities/#{id}?access_token=#{@access_token}"
 
-			response = HTTParty.get(params)
-			body = JSON.parse response.body
-
-			return body
+			response = HTTParty.get(params, headers: HEADERS)
+			JSON.parse response.body
 		end
 
 	end
